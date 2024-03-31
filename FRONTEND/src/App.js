99b99d150f08effd81a12c92c3099bc6812f833c
Thiago <oliveiratolp@gmail.com>
2024-03-31 16:39:38 -0300

    arquivos tela de cadastro

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navigation from './Navigation';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+
+
+export default App;
